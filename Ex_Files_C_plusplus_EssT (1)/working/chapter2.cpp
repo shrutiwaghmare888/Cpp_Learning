15a44fb5c990ccde236cd4a793b3da765a1f7804
shrutiwaghmare888 <shrutiwaghmare105@gmail.com>
2026-06-21 17:15:40 +0530

    function

--- Ex_Files_C_plusplus_EssT (1)/working/chapter2.cpp
+++ Ex_Files_C_plusplus_EssT (1)/working/chapter2.cpp
@@ -3,18 +3,49 @@ using namespace std;
 int main()
 {
     
-     int x{20};
-    int y{7};
-    if(x>y)
-    {
-        cout << "x is greater than y" << endl;
-    }
-    else 
-    {
-        cout << "x is not greater than y" << endl;
-    }
+    //  int x{20};
+    // int y{7};
+    // if(x>y)
+    // {
+    //     cout << "x is greater than y" << endl;
+    // }
+    // else 
+    // {
+    //     cout << "x is not greater than y" << endl;
+    // }
 
    
-    auto z=x<y?" xis less than y": " y is greater than x";
-    cout << z << endl;
+    // auto z=x<y?" xis less than y": " y is greater than x";
+    // cout << z << endl;
+
+
+    //loops
+    //loop end until the condition become false
+
+    int array[]{1,2,3,4,5};
+    int i{0};
+    while(i<5){
+        if (i==3) break;
+        cout<<"Array element :"<<array[i]<<endl;
+        ++i;
+    }
+
+
+    do {
+        cout<<"Array element :"<<array[i]<<endl;
+        ++i;
+    } while(i<5);
+
+
+    // for loop 
+
+    for (int i{0}; i<5; i++)
+    { 
+        cout<<"Array element :"<<array[i]<<endl;
+    }
+    string myname="shruti";
+    for (int i{0}; i<myname.length(); i++)
+    {
+        cout << "Character : " << myname[i] << endl;
+    }
 }
---
+++ Ex_Files_C_plusplus_EssT (1)/working/func1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+void myfunc(int rollno, string name)
+{
+   cout<<"roll no :"<<rollno<<" name :"<<name<<endl;
+}
+int main ()
+{
+    myfunc(1,"shruti");
+    return 0;
+}
